8b1d6e08e19f3f037bc4e121487a2d250ccbafeb
Mike <mikejinhua@163.com>
2017-08-13 21:45:18 +0800

    1
    1

--- Server/src/server/mysql/mysql.go
+++ Server/src/server/mysql/mysql.go
@@ -13,7 +13,7 @@ var (
 
 func OpenDB() {
 	fmt.Println("mysqldb->open db")
-	db1, err := gorm.Open("mysql", "mike:123456@tcp(localhost:3306)/gorm?parseTime=true")
+	db1, err := gorm.Open("mysql", "mike:123456@tcp(localhost:3306)/poker?parseTime=true")
 	if err != nil {
 		panic("connect db error")
 	}
