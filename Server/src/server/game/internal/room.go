6c6763bac7da33bc8337f68ac9b3afc0154426c9
Mike <mikejinhua@163.com>
2017-08-14 19:10:41 +0800

    1
    1

--- Server/src/server/game/internal/room.go
+++ Server/src/server/game/internal/room.go
@@ -1,32 +1,19 @@
 package internal
 
 import (
-	"github.com/name5566/leaf/module"
-	"github.com/name5566/leaf/chanrpc"
-	"server/base"
 	"github.com/liangdas/mqant/gate"
 	"fmt"
-	"server/conf"
-)
-var(
-	roomPlayers = make(map[uint]gate.Agent)
 )
 
 type Room struct{
-
-	roomChanRPC   *chanrpc.Server
 	roomID uint
+	roomPlayers map[uint]gate.Agent
 }
 
 func   newRoom(roomID uint) *Room{
 	fmt.Println("newRoom",roomID)
-	room := Room{roomChanRPC:chanrpc.NewServer(conf.ChanRPCLen)}
+	room := Room{roomID:roomID}
 
-	room.registerChanRPC()
-	room.roomID = roomID
 	return  &room
 }
 
-func JoinRoom(agent gate.Agent)  {
-	roomPlayers[uint(1)] = agent
-}
