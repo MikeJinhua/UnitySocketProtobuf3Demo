f1e4ed7e1bd0193d34e85b134428b849e846e3f1
Mike <mikejinhua@gmail.com>
2018-01-02 09:54:51 +0800

    Update AppConst.cs

--- Client/Assets/Scripts/AppConst.cs
+++ Client/Assets/Scripts/AppConst.cs
@@ -3,5 +3,5 @@ using System.Collections;
 
 public class AppConst {
     public static int SocketPort = 3563;                           //Socket服务器端口
-    public static string SocketAddress = "192.168.3.188";          //Socket服务器地址
+    public static string SocketAddress = "127.0.0.1";          //Socket服务器地址
 }
