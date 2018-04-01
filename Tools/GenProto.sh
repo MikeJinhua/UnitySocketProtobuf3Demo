5d596c3d97d9e4bfa549fb02bd881e5a05c952e3
Mike <mikejinhua@gmail.com>
2018-04-01 15:01:57 +0800

    mac
    mac

---
+++ Tools/GenProto.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+protoc --proto_path=./Proto/src  --csharp_out=../Client/Assets/Scripts/Net/proto ./Proto/src/*.proto
+protoc --proto_path=./Proto/src --go_out=../Server/src/server/msg ./Proto/src/*.proto
+
+python3 gen_proto.py
+pause
---
+++ Tools/GenTableData.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 gen_table_data.py
+pause
